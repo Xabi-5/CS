91e467d2b5d82b92d74e396a44bf2e47b6a3cdab
a22javierlm <a22javierlm@iessanclemente.net>
2023-11-20 13:39:03 +0100

    dsfsdf

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     spl_autoload_register(function ($class) {
         require __DIR__ . "/src/$class.php";
     });
+
+    set_exception_handler("ErrorHandler::handleException");
     header("Content-type: application/json; charset=UTF-8");
+
     $parts = explode("/", $_SERVER['REQUEST_URI']);
     if($parts[1] != "products"){
         http_response_code(404);
@@ -12,6 +15,14 @@
     }
     $id = $parts[2] ?? null;
 
-    $controller = new ProductController;
+    $database = new Database("localhost", "product_db", "appuser", "abc123.");
+
+    $database->getConnection();
+    
+    $gateway = new ProductGateway($database);
+
+
+    $controller = new ProductController($gateway);
 
     $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
+
